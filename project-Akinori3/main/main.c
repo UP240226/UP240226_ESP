360c057a2da33fb877972b2d597d00a3d0da0c6b
Akinori <up240226@alumnos.upa.edu.mx>
2025-10-22 12:39:14 -0600

    panic

--- project-Akinori3/main/main.c
+++ project-Akinori3/main/main.c
@@ -7,6 +7,10 @@
 // Definición de los pines para el LED y el botón
 #define LED GPIO_NUM_23
 #define BUTTON GPIO_NUM_19
+#define INT_PIN GPIO_NUM_5 // Define el pin GPIO5 como pin de interrupción
+uint16_t int_count = 0; // Variable para contar interrupciones
+bool button_state = false; // Bandera para indicar si el botón fue presionado
+
 
  // Duraciones en milisegundos para el código Morse
         #define DOT_MS 200
@@ -43,15 +47,13 @@
         }
         
 
-#define INT_PIN GPIO_NUM_5 // Define el pin GPIO5 como pin de interrupción
-uint16_t int_count = 0; // Variable para contar interrupciones
-bool button_state = false; // Bandera para indicar si el botón fue presionado
 
 // Manejador de la interrupción externa
 static void IRAM_ATTR gpio_isr_handler(void *arg)
 {
-    int_count ++; // Incrementa el contador de interrupciones
+    int_count++; // Incrementa el contador de interrupciones
     button_state = true; // Indica que el botón fue presionado
+    vTaskDelay(pdMS_TO_TICKS(10)); // Pequeña demora para evitar rebotes
 }
 
 // Función principal del programa
@@ -70,10 +72,13 @@ void app_main(void)
 
     while(1) // Bucle principal
     {
-        if(button_state == true) // Si se detectó una pulsación
+        if(int_count > 3) // Si se detectó una pulsación
         {
+            printf("Interrupción detectada\n");
             printf("%d\n", int_count); // Muestra el contador por consola
             button_state = false; // Reinicia la bandera
+            sos(); // Envía el mensaje SOS en código Morse
+            int_count = 0; // Reinicia el contador
         }
         vTaskDelay(pdMS_TO_TICKS(100)); // Espera 100 ms antes de repetir
     }
